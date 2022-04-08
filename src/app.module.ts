915cb02911414a26462e3325e1b90c3181dd26d3
Franck Fernandez <franck.fernandez@gmail.com>
2022-04-08 12:13:04 +0200

    Update Graphql config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,15 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
