b8e3856dfc63835e797250d9a2eadecfbe1de6d8
Franck Fernandez <franck.fernandez@gmail.com>
2022-04-08 18:01:32 +0200

    Implement real JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { User } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(username: string, password: string): Promise<any> {
     const user = this.usersService.findOne(username);
@@ -20,7 +24,10 @@ export class AuthService {
   login(user: User) {
     return {
       user,
-      accessToken: 'fake-token',
+      accessToken: this.jwtService.sign({
+        username: user.username,
+        sub: user.id,
+      }),
     };
   }
 }
