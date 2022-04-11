c5b739d1e726a5cd6ad7a6ed8055d8ebfdb1695e
Franck Fernandez <franck.fernandez@gmail.com>
2022-04-11 17:46:10 +0200

    Add signup mutation and service + Implement bcrypt for password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { User } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import { LoginUserInput } from './dto/login-user.input';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -12,8 +14,9 @@ export class AuthService {
 
   async validateUser(username: string, password: string): Promise<any> {
     const user = this.usersService.findOne(username);
+    const valid = await bcrypt.compare(password, user?.password);
 
-    if (user && user.password === password) {
+    if (user && valid) {
       const { password, ...result } = user;
 
       return result;
@@ -29,5 +32,17 @@ export class AuthService {
         sub: user.id,
       }),
     };
+  }
+
+  async signup(loginUserInput: LoginUserInput) {
+    const user = this.usersService.findOne(loginUserInput.username);
+
+    if (user) {
+      throw new Error('User already exists');
+    }
+
+    const password = await bcrypt.hash(loginUserInput.password, 10);
+
+    return this.usersService.create({ ...loginUserInput, password });
   }
 }
